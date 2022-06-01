8f5784f80322cfc086145d9a199c0d8ad06ffb45
Bolio <again.bolio@hotmail.com>
2022-06-01 14:02:06 -0500

    Add icons (star and bell) of Material Icons in Actions component.

--- movements-rick-and-morty/src/components/Actions/index.tsx
+++ movements-rick-and-morty/src/components/Actions/index.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import StarIcon from "@mui/icons-material/Star";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const ActionsStyled = styled.div`
   display: flex;
@@ -6,12 +8,37 @@ const ActionsStyled = styled.div`
   height: 116px;
   margin-bottom: 182px;
   background-color: purple;
+  justify-content: center;
+  div {
+    position: relative;
+    width: 100%;
+    top: 36px;
+    svg {
+      position: absolute;
+      width: 21px;
+      height: 20px;
+      fill: white;
+      :nth-child(1) {
+        left: 69px;
+      }
+      :nth-child(2) {
+        left: 176px;
+      }
+      :nth-child(3) {
+        right: 74px;
+      }
+    }
+  }
 `;
 
 const Actions = () => {
   return (
     <ActionsStyled>
-      <h1>ACTIONS</h1>
+      <div>
+        <StarIcon />
+        <StarIcon />
+        <NotificationsIcon />
+      </div>
     </ActionsStyled>
   );
 };
