ac4f9ccfa95f740130fbdc297bd44059044fc508
Bolio <again.bolio@hotmail.com>
2022-06-03 20:08:54 -0500

    Add types in the duck of redux.

--- movements-rick-and-morty/src/redux/modules/listMovements/index.ts
+++ movements-rick-and-morty/src/redux/modules/listMovements/index.ts
@@ -1,4 +1,15 @@
-const initialState = {
+import { Dispatch } from "redux";
+
+interface RickAndMortyState {
+  listMovements: [];
+  fetching: boolean;
+  success: boolean;
+  error: boolean;
+  errorMessage: string;
+  meta: {};
+}
+
+const initialState: RickAndMortyState = {
   listMovements: [],
   fetching: true,
   success: false,
@@ -7,21 +18,41 @@ const initialState = {
   meta: {},
 };
 
+enum ActionType {
+  FETCH_LIST_MOVEMENTS = "modules/listMovements/FETCH_LIST_MOVEMENTS",
+  ERROR_LIST_MOVEMENTS = "modules/currencyConverter/ERROR_LIST_MOVEMENTS",
+}
+
+interface actionFetchListMovements {
+  type: ActionType.FETCH_LIST_MOVEMENTS;
+  payload: { info: {}; results: [] };
+}
+
+interface actionErrorListMovements {
+  type: ActionType.ERROR_LIST_MOVEMENTS;
+  payload: string | object;
+}
+
+type Action = actionFetchListMovements | actionErrorListMovements;
+
 // Actions
-const FETCH_LIST_MOVEMENTS = "modules/listMovements/FETCH_LIST_MOVEMENTS";
-const ERROR_LIST_MOVEMENTS = "modules/currencyConverter/ERROR_LIST_MOVEMENTS";
+// const FETCH_LIST_MOVEMENTS = "modules/listMovements/FETCH_LIST_MOVEMENTS";
+// const ERROR_LIST_MOVEMENTS = "modules/currencyConverter/ERROR_LIST_MOVEMENTS";
 
 // Reducers
-export default function reducer(state = initialState, action = {}) {
+export default function reducer(
+  state: RickAndMortyState = initialState,
+  action: Action
+): RickAndMortyState {
   switch (action.type) {
-    case FETCH_LIST_MOVEMENTS:
+    case ActionType.FETCH_LIST_MOVEMENTS:
       return {
         ...state,
         listMovements: action.payload.results,
         meta: action.payload.info,
         fetching: false,
       };
-    case ERROR_LIST_MOVEMENTS:
+    case ActionType.ERROR_LIST_MOVEMENTS:
       return {
         ...state,
         fetching: false,
@@ -34,34 +65,34 @@ export default function reducer(state = initialState, action = {}) {
 }
 
 // Action Creators
-export const fetchListMovements = (payload) => {
+export const fetchListMovements = (payload: { info: {}; results: [] }) => {
   return {
-    type: FETCH_LIST_MOVEMENTS,
+    type: ActionType.FETCH_LIST_MOVEMENTS,
     payload,
   };
 };
 
-export const errorListMovements = (payload) => {
+export const errorListMovements = (payload: string) => {
   return {
-    type: ERROR_LIST_MOVEMENTS,
+    type: ActionType.ERROR_LIST_MOVEMENTS,
     payload,
   };
 };
 
 // Thunk
-export const fetchListMovementsThunk = () => async (dispatch) => {
+export const fetchListMovementsThunk = () => async (dispatch: Dispatch) => {
   try {
     console.log("FROM API");
     const urlAPI = "https://rickandmortyapi.com/api/character";
     const responseAPI = await fetch(urlAPI);
     const resultAPI = await responseAPI.json();
     if (resultAPI.error) {
-      dispatch(errorListMovements(resultAPI.error));
+      dispatch(errorListMovements(resultAPI?.error));
     } else {
       dispatch(fetchListMovements(resultAPI));
     }
-  } catch (error) {
-    dispatch(errorListMovements(error));
-    console.log(error);
+  } catch (err: any) {
+    dispatch(errorListMovements(err));
+    console.error(err);
   }
 };
