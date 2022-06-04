b55784ed701fc99e0f57e2d7ef39c676d96e0b2e
Bolio <again.bolio@hotmail.com>
2022-06-04 17:54:23 -0500

    Add types in the duck of redux.

--- movements-rick-and-morty/src/redux/modules/listMovements/index.ts
+++ movements-rick-and-morty/src/redux/modules/listMovements/index.ts
@@ -18,26 +18,38 @@ const initialState: RickAndMortyState = {
   meta: {},
 };
 
+// Actions
 enum ActionType {
   FETCH_LIST_MOVEMENTS = "modules/listMovements/FETCH_LIST_MOVEMENTS",
   ERROR_LIST_MOVEMENTS = "modules/currencyConverter/ERROR_LIST_MOVEMENTS",
 }
+// const FETCH_LIST_MOVEMENTS = "modules/listMovements/FETCH_LIST_MOVEMENTS";
+// const ERROR_LIST_MOVEMENTS = "modules/currencyConverter/ERROR_LIST_MOVEMENTS";
 
+// Action Creator
 interface actionFetchListMovements {
   type: ActionType.FETCH_LIST_MOVEMENTS;
   payload: { info: {}; results: [] };
 }
-
 interface actionErrorListMovements {
   type: ActionType.ERROR_LIST_MOVEMENTS;
   payload: string | object;
 }
+// export const fetchListMovements = (payload: { info: {}; results: [] }) => {
+//   return {
+//     type: ActionType.FETCH_LIST_MOVEMENTS,
+//     payload,
+//   };
+// };
+
+// export const errorListMovements = (payload: string) => {
+//   return {
+//     type: ActionType.ERROR_LIST_MOVEMENTS,
+//     payload,
+//   };
+// };
 
 type Action = actionFetchListMovements | actionErrorListMovements;
-
-// Actions
-// const FETCH_LIST_MOVEMENTS = "modules/listMovements/FETCH_LIST_MOVEMENTS";
-// const ERROR_LIST_MOVEMENTS = "modules/currencyConverter/ERROR_LIST_MOVEMENTS";
 
 // Reducers
 export default function reducer(
@@ -64,35 +76,30 @@ export default function reducer(
   }
 }
 
-// Action Creators
-export const fetchListMovements = (payload: { info: {}; results: [] }) => {
-  return {
-    type: ActionType.FETCH_LIST_MOVEMENTS,
-    payload,
-  };
-};
-
-export const errorListMovements = (payload: string) => {
-  return {
-    type: ActionType.ERROR_LIST_MOVEMENTS,
-    payload,
-  };
-};
-
 // Thunk
-export const fetchListMovementsThunk = () => async (dispatch: Dispatch) => {
-  try {
-    console.log("FROM API");
-    const urlAPI = "https://rickandmortyapi.com/api/character";
-    const responseAPI = await fetch(urlAPI);
-    const resultAPI = await responseAPI.json();
-    if (resultAPI.error) {
-      dispatch(errorListMovements(resultAPI?.error));
-    } else {
-      dispatch(fetchListMovements(resultAPI));
+export const fetchListMovementsThunk =
+  () => async (dispatch: Dispatch<Action>) => {
+    try {
+      console.log("FROM API");
+      const urlAPI = "https://rickandmortyapi.com/api/character";
+      const responseAPI = await fetch(urlAPI);
+      const resultAPI = await responseAPI.json();
+      if (resultAPI.error) {
+        dispatch({
+          type: ActionType.ERROR_LIST_MOVEMENTS,
+          payload: resultAPI?.error,
+        });
+      } else {
+        dispatch({
+          type: ActionType.FETCH_LIST_MOVEMENTS,
+          payload: resultAPI,
+        });
+      }
+    } catch (err: any) {
+      dispatch({
+        type: ActionType.ERROR_LIST_MOVEMENTS,
+        payload: err,
+      });
+      console.error(err);
     }
-  } catch (err: any) {
-    dispatch(errorListMovements(err));
-    console.error(err);
-  }
-};
+  };
