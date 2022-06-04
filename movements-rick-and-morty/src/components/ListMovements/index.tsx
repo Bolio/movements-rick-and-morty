b02c0d21dab3cf12823994bed63ccc39dab67a5f
Bolio <again.bolio@hotmail.com>
2022-06-04 18:39:23 -0500

    Refactor in ListMovements component.

--- movements-rick-and-morty/src/components/ListMovements/index.tsx
+++ movements-rick-and-morty/src/components/ListMovements/index.tsx
@@ -26,14 +26,14 @@ const ListMovements = () => {
     movements: RickAndMortyState;
   }
 
-  const characters: [] = useSelector((state: RootState) => state.movements.listMovements);
-  const fetch: boolean = useSelector((state: RootState) => state.movements.fetching);
-  const error: boolean = useSelector((state: RootState) => state.movements.error);
-  const errorMessage: string = useSelector(
-    (state: RootState) => state.movements.errorMessage
-  );
+  const {
+    listMovements: characters,
+    fetching,
+    error,
+    errorMessage,
+  }: RickAndMortyState = useSelector((state: RootState) => state.movements);
 
-  if (fetch) {
+  if (fetching) {
     return (
       <Text fontSize="32px" textAlign="center" top="30px">
         Loading...
