b75fa6e1b03d4c5c727183256557a16322cf5cc6
Bolio <again.bolio@hotmail.com>
2022-06-05 19:29:25 -0500

    Add theme.ts file

---
+++ movements-rick-and-morty/src/styled/themes/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    blue: "#5acaee",
+    darkBlue: "#22a8d2",
+    purple: "#bb64c8",
+    darkPurple: "#783183",
+    black: "#333333",
+  },
+  fontWeight: {
+    light: 300,
+    medium: "normal",
+    bolder: 600,
+  },
+  fontSize: {
+    xSmall: "0.5rem", // 8px
+    small: "0.75rem", // 12px
+    medium: "0.875rem", // 14px
+    normal: "1rem", // 16px
+    large: "1.125rem", // 18px
+    xLarge: "1.75rem", // 28px
+    xxLarge: "2.125rem", // 34px
+  },
+};
